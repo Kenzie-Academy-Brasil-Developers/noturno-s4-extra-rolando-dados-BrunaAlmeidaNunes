ec9868c81fc9010e06bffa0cee5134f737673e53
Bruna Almeida Nunes da Conceicao <brunamamamu@gmail.com>
2022-01-14 17:49:26 -0300

    alteraçoes

---
+++ script.js
@@ -0,0 +1,43 @@
+const conteudo = document.getElementById("conteudo")
+const tabela = document.getElementById("tabela")
+const barras = document.getElementById("barras")
+
+let dado1 = 0
+let dado2 = 0
+let soma = 0
+let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+
+const rolar = () => {
+    for(i=0; i<1000; i++){
+        dado1 = Math.floor(Math.random() * 6 + 1)
+        dado2 = Math.floor(Math.random() * 6 + 1)
+        soma = dado1 + dado2
+        count[soma-2]++
+    }
+    for(i=0; i<11; i++){
+        const div = document.createTextNode( i+2 + ": " + count[i] )
+        tabela.appendChild(div)
+        const br = document.createElement("br")
+        tabela.appendChild(br)
+    }
+    for(i=0; i<11; i++){
+        const minhaBarra = document.createElement("div")
+        barras.appendChild(minhaBarra)
+        minhaBarra.style.backgroundColor = "#2E8B57"
+        minhaBarra.style.height = "20px"
+        minhaBarra.style.width = count[i] + "px"
+        const space = document.createElement("br")
+        barras.appendChild(space)
+		
+    }
+	
+    return count
+	
+}
+
+const botaoRolar = document.getElementById("botao");
+
+botaoRolar.addEventListener("click", rolar);
+
